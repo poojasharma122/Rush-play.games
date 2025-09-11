6bb40a8b7f37ad3a4d27bf662cc625013f97f981
frontenddev <frontendev344@gmail.com>
2025-09-11 19:15:07 +0530

    fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,3 +32,29 @@ $(window).mousemove(function (e) {
 });
 
 // cursor js end
+
+
+// fqas start
+
+const accordionItemh = document.querySelectorAll(".ko-accordion-item-header");
+accordionItemh.forEach((accordionItemh) => {
+  accordionItemh.addEventListener("click", (event) => {
+    // Uncomment in case you only want to allow for the display of only one collapsed item at a time!
+
+    //     const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item-header.active");
+    //     if(currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader!==accordionItemHeader) {
+    //        currentlyActiveAccordionItemHeader.classList.toggle("active");
+    //        currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
+    //      }
+    accordionItemh.classList.toggle("active");
+    const accordionItemBody = accordionItemh.nextElementSibling;
+    if (accordionItemh.classList.contains("active")) {
+      accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+    } else {
+      accordionItemBody.style.maxHeight = 0;
+    }
+  });
+});
+
+
+// faqs end
